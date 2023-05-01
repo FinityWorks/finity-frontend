7ca80ed5a2a5337f8f56fae9556f31e639731c8d
NathanDBrowne <nathan.browne1@gmail.com>
2023-05-01 19:23:06 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
+
+const data = [
+  {
+    x: ["2022-01-01", "2022-01-02", "2022-01-03", "2022-01-04"],
+    open: [100, 110, 120, 130],
+    high: [120, 130, 140, 150],
+    low: [90, 100, 110, 120],
+    close: [110, 120, 130, 140],
+    type: "candlestick",
+    name: "Candlestick Chart",
+  },
+];
+
+const layout = {
+  title: "Bars",
+  xaxis: {
+    title: "Index",
+  },
+  yaxis: {
+    title: "Price",
+  },
+};
+
+function CandlestickChart({ data, layout }) {
+  return <Plot data={data} layout={layout} />;
+}
 
 function App() {
+  const [bars, setBars] = useState({});
+  const [layout, setLayout] = useState({});
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/tick_imbal/")
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data.layout);
+        setBars([data.bars]);
+        setLayout(data.layout);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CandlestickChart data={bars} layout={layout} />
     </div>
   );
 }
