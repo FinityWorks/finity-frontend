bd5873267ca46f23f16ae33fb155629e6a63cc1d
NathanDBrowne <nathan.browne1@gmail.com>
2023-05-05 20:54:11 +0100

    create websocket connection

--- src/App.js
+++ src/App.js
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
 import Plot from "react-plotly.js";
+import { w3cwebsocket as WebSocket } from "websocket";
 
-const data = [
-  {
-    x: ["2022-01-01", "2022-01-02", "2022-01-03", "2022-01-04"],
-    open: [100, 110, 120, 130],
-    high: [120, 130, 140, 150],
-    low: [90, 100, 110, 120],
-    close: [110, 120, 130, 140],
-    type: "candlestick",
-    name: "Candlestick Chart",
-  },
-];
-
-const layout = {
-  title: "Bars",
-  xaxis: {
-    title: "Index",
-  },
-  yaxis: {
-    title: "Price",
-  },
-};
+const socketUrl = "ws://127.0.0.1:8000/ws";
+const socket = new WebSocket(socketUrl);
 
 function CandlestickChart({ data, layout }) {
+  layout = Object.assign({}, layout, {
+    width: 2300,
+    height: 1000,
+    paper_bgcolor: "#222",
+    plot_bgcolor: "#222",
+    font: { color: "#fff" },
+    xaxis: {
+      gridcolor: "white",
+      gridwidth: 1,
+      rangeslider: { visible: false },
+      title: "Bar ID",
+      titlefont: {
+        family: "Arial",
+        size: 18,
+        color: "#fff",
+      },
+    },
+    yaxis: {
+      gridcolor: "white",
+      gridwidth: 1,
+      title: "Price",
+      titlefont: {
+        family: "Arial",
+        size: 18,
+        color: "#fff",
+      },
+    },
+  });
   return <Plot data={data} layout={layout} />;
 }
 
 function App() {
-  const [bars, setBars] = useState({});
+  const [bars, setBars] = useState([]);
   const [layout, setLayout] = useState({});
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/tick_imbal/")
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data.layout);
-        setBars([data.bars]);
-        setLayout(data.layout);
-      });
+    socket.onopen = () => console.log("socket connected");
+    socket.onclose = () => console.log("socket disconnected");
+    socket.onmessage = (event) => {
+      const newData = JSON.parse(event.data);
+      setBars([newData.bars]);
+      setLayout(newData.layout);
+    };
+
+    return () => {
+      socket.close();
+    };
   }, []);
 
   return (
     <div className="App">
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h1 style={{ color: "#fff" }}>Live prices</h1>
+      </div>
       <CandlestickChart data={bars} layout={layout} />
     </div>
   );
